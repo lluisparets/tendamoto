78241c222cda9e857df19a4c937a6e40739c18a5
MIguel <mobradorr@esliceu.net>
2021-05-13 10:48:53 +0200

    0.0.13

--- insert_api_proveidor.php
+++ insert_api_proveidor.php
@@ -1,5 +1,6 @@
 <?php 
 require 'includes/head.php'; 
+
 echo "<p>" . $_POST['Nom'] . "</p>";
 echo "<p>" . $_POST['Adreca'] . "</p>";
 echo "<p>" . $_POST['Telefon'] . "</p>";
@@ -8,7 +9,7 @@ $nom = $_POST['nom'];
 $adreca = $_POST['adreca'];
 $telefon = $_POST['telefon'];
 
-$query = "INSERT INTO Proveidor (nom,adreca,telefon) VALUES (\"$nom\",\"$adreca\",\"$telefon\");"
+$query = "INSERT INTO Proveidor (Nom,Adreca,Telefon) VALUES (\"$nom\",\"$adreca\",\"$telefon\")";
 echo "<p>" . $query . "</p>";
 
 $res = mysqli_query($bbdd, $query);
