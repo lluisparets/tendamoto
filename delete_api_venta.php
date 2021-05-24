64cd14dfd210b29f4f43e93bd0733d2452822d4d
MIguel <mobradorr@esliceu.net>
2021-05-24 13:32:49 +0200

    0.1.2

---
+++ delete_api_venta.php
@@ -0,0 +1,11 @@
+<?php
+require "includes/head.php";
+$query="delete from moto where idVenta=\"$_GET[id]\" ";
+$result=mysqli_query($bbdd,$query);
+
+if($result){
+    header('Location: ok.php');
+}else{
+    $error = mysqli_error($bbdd);
+    header("Location: no_ok.php?error=$error");
+}
---
+++ llista_venta.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="es-ES">
+<?php require 'includes/head.php'; ?>
+
+<body style="background-color: #fff59d;">
+    <?php require 'includes/header.php'; ?>
+
+    <h1 style="color:blue;">Llista Ventes</h1>
+
+    <form action="llista_venta.php" method="get" style="margin-bottom: 40px;"> 
+        <Label><b>Filtrar per Venta</b></Label>
+        <select required name="Venta">
+            <option value="1">Venta 1</option>
+        </select>
+        <button type="submit">Filtrar</button>
+    </form>
+
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Data</th>
+                <th>Preu</th>
+                <th>Compra</th>
+                <th>Client</th>
+                <th>Moto</th>
+            </tr>
+           
+        </thead>
+
+        <tbody>
+            <?php
+            $where = '';
+            if(isset($_GET['venta'])){
+                $where = " WHERE idVenta = '$_GET[Venta]' ";
+            }
+            $query = "
+            select V.*, M.Marca as nom_moto, C.firstname as nom_client, C.lastname as ll_client
+            from Venta as V INNER JOIN Moto as M ON (M.idMoto = V.fkidmoto) 
+            INNER JOIN Client as C ON (C.idclient = V.fkidclient) 
+            $where";
+            $result = mysqli_query($bbdd, $query) or die(mysqli_error($bbdd));
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>
+                            <td> $row[idVenta] </td>
+                            <td> $row[Fecha] </td>
+                            <td> $row[Preu] </td>
+                            <td> $row[Compra] </td>
+                            <td> $row[nom_client] $row[ll_client] </td>
+                            <td> $row[nom_moto] </td>
+                            <td><a href=\"delete_api_venta.php?id=$row[idVenta]\">ELIMINAR</a></td>
+
+                        </tr>";
+            }
+            ?>
+
+        </tbody>
+    </table>
+
+</body>
+
+</html>
